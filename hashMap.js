a6f21b3910740b77aba203f62edf33e83db61480
Guy Moreno <173142004+GuyMoreno@users.noreply.github.coml173142004+GuyMoreno@users.noreply.github.com>
2025-07-20 17:02:45 +0300

    added all functionality

--- hashMap.js
+++ hashMap.js
@@ -17,6 +17,27 @@ export class HashMap {
     return hashCode;
   }
 
+  resize() {
+    // Double the capacity
+    this.capacity *= 2;
+
+    // Store current data temporarily
+    const oldBuckets = this.buckets;
+
+    // Create new empty buckets
+    this.buckets = new Array(this.capacity).fill(null).map(() => []);
+
+    // Reset size (will be recalculated as we insert again)
+    this.size = 0;
+
+    // Re-insert all existing pairs into the new buckets
+    for (const bucket of oldBuckets) {
+      for (const [key, value] of bucket) {
+        this.set(key, value); // This will hash them again into new buckets
+      }
+    }
+  }
+
   set(key, value) {
     const index = this.hash(key);
     const bucket = this.buckets[index];
@@ -36,16 +57,87 @@ export class HashMap {
       this.resize();
     }
   }
+
+  get(key) {
+    const index = this.hash(key);
+    const bucket = this.buckets[index];
+
+    for (let i = 0; i < bucket.length; i++) {
+      const [existingKey, value] = bucket[i];
+
+      if (existingKey === key) {
+        return value;
+      }
+    }
+    return null;
+  }
+
+  has(key) {
+    const index = this.hash(key);
+    const bucket = this.buckets[index];
+
+    for (let i = 0; i < bucket.length; i++) {
+      const [existingKey] = bucket[i];
+      if (existingKey === key) return true;
+    }
+
+    return false;
+  }
+
+  remove(key) {
+    const index = this.hash(key);
+    const bucket = this.buckets[index];
+
+    for (let i = 0; i < bucket.length; i++) {
+      const [existingKey] = bucket[i];
+      if (existingKey === key) {
+        bucket.splice(i, 1); // remove the [key, value] pair
+        return true;
+      }
+    }
+
+    return false;
+  }
+  length() {
+    return this.size;
+  }
+
+  clear() {
+    this.buckets = new Array(this.capacity).fill(null).map(() => []);
+    this.size = 0;
+  }
+
+  keys() {
+    const allKeys = [];
+
+    for (const bucket of this.buckets) {
+      for (const [key] of bucket) {
+        allKeys.push(key);
+      }
+    }
+
+    return allKeys;
+  }
+
+  values() {
+    const allValues = [];
+
+    for (let bucket of this.buckets) {
+      for (let [key, value] of bucket) {
+        allValues.push(value);
+      }
+    }
+    return allValues;
+  }
+
+  entries() {
+    const allEntries = [];
+
+    for (let bucket of this.buckets) {
+      for (let entry of bucket) {
+        allEntries.push(entry);
+      }
+    }
+    return allEntries;
+  }
 }
-
-// HashMap (capacity = 4)
-
-// +------------+
-// | Bucket[0]  | --> [ ['Alice', {...}], ['Eli', {...}] ]
-// +------------+
-// | Bucket[1]  | --> [ ['Bob', {...}] ]
-// +------------+
-// | Bucket[2]  | --> [ ['Charlie', {...}] ]
-// +------------+
-// | Bucket[3]  | --> [ ['Dana', {...}] ]
-// +------------+
--- main.js
+++ main.js
@@ -1,10 +1,4 @@
 // import { hash } from "./hash.js";
 import { HashMap } from "./hashMap.js";
 
-const map = new HashMap(4, 0.75);
 
-// let c = new Array(4).fill(null).map(() => []);
-// console.log(c);
-
-// let d = new Array(4).fill([]);
-// console.log(d);
